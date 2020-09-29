b0d3930d07050e150c29c477adee458c884fc4b7
Aaron Bester <bestera@uoguelph.ca>
2020-09-29 16:49:33 -0400

    Added comments

--- array.c
+++ array.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "array.h"
 
+/*Mallocs a new performace structure and sets everything to the default values*/
 struct Performance * newPerformance(){
     struct Performance * emptyPerformance = malloc(sizeof(struct Performance) * 1);
     emptyPerformance->reads = 0;
@@ -10,19 +11,19 @@ struct Performance * newPerformance(){
     emptyPerformance->frees = 0;
     return(emptyPerformance);
 }
-
+/*Mallocs a new array structure and sets everything to the default values*/
 struct Array * newArray(struct Performance * performance, unsigned int width, unsigned int capacity){
     struct Array * emptyArray = malloc(sizeof(struct Array));
 
     if(emptyArray == NULL){
-        fprintf(stderr,"Error Not enough memory to malloc a array\n");
+        fprintf(stderr,"Error: Not enough memory to malloc a array\n");
         exit(1);
     }
 
     emptyArray->data = malloc(sizeof(char) * (width * capacity));
 
     if(emptyArray->data == NULL){
-        fprintf(stderr,"Error Not enough memory to malloc data in an array\n");
+        fprintf(stderr,"Error: Not enough memory to malloc array->data\n");
         exit(1);
     }
 
@@ -32,7 +33,7 @@ struct Array * newArray(struct Performance * performance, unsigned int width, un
     performance->mallocs++;
     return(emptyArray);
 }
-
+/*Reads a item from the array into a destination pointer*/
 void readItem(struct Performance * performance, struct Array * array, unsigned int index, void * dest){
     int numOfBytes = 0;
     int startLocation = (index * array->width); 
@@ -52,7 +53,7 @@ void readItem(struct Performance * performance, struct Array * array, unsigned i
     dest = destArray;
     performance->reads++;
 }
-
+/*Writes a item from a src pointer to the array structure */
 void writeItem(struct Performance * performance, struct Array * array, unsigned int index, void * src){
 
     int numOfBytes = 0;
@@ -76,7 +77,7 @@ void writeItem(struct Performance * performance, struct Array * array, unsigned 
     }
     performance->writes++;
 }
-
+/*Takes the array number of elements and subtracts one */
 void contract(struct Performance * performance, struct Array * array){
     if(array->nel <= 0){
         fprintf(stderr,"Error: Can't contract a array with nothing in it.\n");
@@ -84,7 +85,7 @@ void contract(struct Performance * performance, struct Array * array){
     }
     array->nel--;
 }
-
+/* Frees the array structure*/
 void freeArray(struct Performance * performance, struct Array * array){
     free(array->data);
     array->data = NULL;
@@ -92,15 +93,16 @@ void freeArray(struct Performance * performance, struct Array * array){
     array = NULL;
     performance->frees++;
 }
-
+/*Add a item to the end of a array structure*/
 void appendItem(struct Performance * performance, struct Array * array, void * src){
     writeItem(performance,array,array->nel,src);
 }
-
+/*Inserts a item into any part of the array structure*/
 void insertItem(struct Performance * performance, struct Array * array, unsigned int index, void * src){
     int i = 0;
     void * itemToBeMoved = malloc(sizeof(char) * array->width);
-       
+    /*Index is cast to a int because if i is less than zero and its compared to a unsigned value of index i will become a massive unsigned value 
+    which will cause the for loop to run until it runs out of room.*/
     for(i = array->nel-1; i >= (int)index; i = i-2){
         readItem(performance,array,i,itemToBeMoved);
         i++;
@@ -111,11 +113,11 @@ void insertItem(struct Performance * performance, struct Array * array, unsigned
     writeItem(performance,array,index,src);
     
 }
-
+/*Adds a item to the start of the array structure*/
 void prependItem(struct Performance * performance, struct Array * array, void * src){
     insertItem(performance,array,0,src);
 }
-
+/*Delets a item from the array structure*/
 void deleteItem(struct Performance * performance ,struct Array * array , unsigned int index){
     int i = 0;
     void * itemToBeMoved = malloc(sizeof(char) * array->width);
@@ -128,7 +130,7 @@ void deleteItem(struct Performance * performance ,struct Array * array , unsigne
     free(itemToBeMoved);
     contract(performance,array);
 }
-
+/*Finds a item in the array structure using a sorted search*/
 int findItem(struct Performance * performance, struct Array * array, int (*compar)(const void *, const void *), void * target){
     void * arrayByte = malloc(sizeof(char) * array->width);
     int i = 0;
@@ -144,7 +146,7 @@ int findItem(struct Performance * performance, struct Array * array, int (*compa
     return(-1);
    
 }
-
+/*Searches for a item in the array structure using a binary search*/
 int searchItem(struct Performance * performance, struct Array * array, int (*compar)(const void *, const void *), void * target){
     void * arrayByte = malloc(sizeof(char) * array->width);
     int first = 0;
